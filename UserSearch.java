1616d21b43873b25056947c744e3b31ca7d35be6
dth27 <dth27@hi.is>
2017-03-13 14:17:41 +0000

    Add files via upload

---
+++ UserSearch.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vinnsla;
+
+/**
+ *
+ * @author Dottedsocks
+ */
+public class UserSearch {
+    
+    public UserSearch(){
+        
+    }
+    public void getFlights(String howMany, String from, String to, String date){
+        
+    }
+    public void sortFlights(){
+        
+    }
+    public void createFlights(){
+        
+    }
+    
+}
